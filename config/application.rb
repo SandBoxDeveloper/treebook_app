51707b7ca1440231582513282bfba7c3e2e96674
Andre Hitchman <madetobethebest@hotmail.co.uk>
2013-11-09 15:15:45 +0000

    updated application.rb

--- config/application.rb
+++ config/application.rb
@@ -65,6 +65,6 @@ module Treebok
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false #default is true
   end
 end
